beb6ab8ab9819970aa3e93b499f726dfc7f75e7e
Marc <marc.henrion5769@gmail.com>
2018-09-12 13:29:59 +0200

    1er Commit de LaFleurMVC

---
+++ vues/v_produits.php
@@ -0,0 +1,26 @@
+<div id="produits">
+<?php
+	
+foreach( $lesProduits as $unProduit) 
+{
+	$id = $unProduit['id'];
+	$description = $unProduit['description'];
+	$prix=$unProduit['prix'];
+	$image = $unProduit['image'];
+        $quantite = $uneProduit['quantite'];
+	?>	
+	<ul>
+			<li><img src="<?php echo $image ?>" alt=image /></li>
+			<li><?php echo $description ?></li>
+			 <li><?php echo " : ".$prix." Euros" ?>
+			<li><a href=index.php?uc=voirProduits&categorie=<?php echo $categorie ?>&produit=<?php echo $id ?>&action=ajouterAuPanier> 
+			 <img src="images/mettrepanier.png" TITLE="Ajouter au panier" </li></a>
+			
+	</ul>
+			
+			
+			
+<?php			
+}
+?>
+</div>
